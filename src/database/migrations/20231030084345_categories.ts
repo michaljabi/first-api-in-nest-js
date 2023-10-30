e72b565d3fc353cb4423af12b428a10778937ad6
Jabi <michal.jabi@gmail.com>
2023-10-30 10:01:55 +0100

    10.3.1 - categories migration.

---
+++ src/database/migrations/20231030084345_categories.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('categories', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable().unique();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('categories');
+}
