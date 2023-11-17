87266cb8aeea026334c51cf7f7820af498e1542f
Jabi <michal.jabi@gmail.com>
2023-11-18 00:59:25 +0100

    3.3.1 Data ready for `/categories`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller()
+interface Category {
+  id: number;
+  name: string;
+}
+
+@Controller('categories')
 export class AppController {
+  private categories: Category[] = [
+    { id: 1, name: 'Groceries' },
+    { id: 2, name: 'Cosmetics' },
+    { id: 3, name: 'Toys' },
+    { id: 4, name: 'Dairy' },
+    { id: 5, name: 'Fashion' },
+    { id: 6, name: 'Electronics' },
+    { id: 7, name: 'Games' },
+  ];
+
   @Get()
-  getHello(): string {
-    return 'Goodbye world';
+  getAll(): Category[] {
+    return this.categories;
   }
 
   @Get('cat/:catId')
