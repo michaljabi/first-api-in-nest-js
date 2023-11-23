8992c24014775bc4cfae16eea95573b6e8d0f5b5
Jabi <michal.jabi@gmail.com>
2023-11-23 06:41:56 +0100

    step: 6.6.1 - new categories.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories/categories.controller';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
+import { CategoriesService } from './categories/categories.service';
 
 @Module({
   imports: [],
   controllers: [CategoriesController, ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, CategoriesService],
 })
 export class AppModule {}
