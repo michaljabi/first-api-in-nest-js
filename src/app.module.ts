864de056ae90f3f3a8a102d4db0f7e998dccf22c
Jabi <michal.jabi@gmail.com>
2023-11-23 05:24:23 +0100

    step: 6.2 generate new service (registered in providers Array)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories/categories.controller';
 import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [],
   controllers: [CategoriesController, ProductsController],
-  providers: [],
+  providers: [ProductsService],
 })
 export class AppModule {}
