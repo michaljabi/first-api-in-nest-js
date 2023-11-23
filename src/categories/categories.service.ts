e5aa5f75a89ccfbd579d233af74746b68c588ef4
Jabi <michal.jabi@gmail.com>
2023-11-23 06:52:31 +0100

    step: 6.6.2 - copy logic to service layer

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Category } from './category.interface';
+import { categoriesList } from './categories-list';
+import { NewCategoryDto } from './dto/new-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  private categories: Category[] = categoriesList;
+  private nextId = 8;
+
+  private find(id: number): Category {
+    const category = this.categories.find((c) => c.id === id);
+    if (!category) {
+      throw new NotFoundException(`category with id: ${id} not found`);
+    }
+    return category;
+  }
+
+  getAll(): readonly Category[] {
+    return this.categories;
+  }
+
+  addNew(categoryDto: NewCategoryDto): Category {
+    const category: Category = { id: this.nextId++, ...categoryDto };
+    this.categories.push(category);
+    return category;
+  }
+
+  getOneById(id: number): Category {
+    return this.find(id);
+  }
+
+  removeById(id: number): { id: number; removed: boolean } {
+    this.find(id);
+    this.categories = this.categories.filter((c) => c.id !== id);
+    return { id, removed: true };
+  }
+}
