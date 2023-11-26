eadf559f45c2f793b0e938ad590e777ef66282b5
Jabi <michal.jabi@gmail.com>
2023-11-26 06:43:00 +0100

    7.3.3 config log levels

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['verbose'],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
   console.log('Server is running on http://localhost:3000');
