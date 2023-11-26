bcb55f5cf6fc2f671d5ca7f0d4520fed587f0807
Jabi <michal.jabi@gmail.com>
2023-11-26 07:28:09 +0100

    7.4 add and config nestjs-pino pino-http

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { CategoriesController } from './categories/categories.controller';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { CategoriesService } from './categories/categories.service';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule.forRoot()],
   controllers: [CategoriesController, ProductsController],
   providers: [ProductsService, CategoriesService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['verbose'],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
   console.log('Server is running on http://localhost:3000');
 }
