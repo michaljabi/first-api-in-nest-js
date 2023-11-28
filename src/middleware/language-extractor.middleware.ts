7e336290be739a338261e18f3d09941fcda4be4c
Jabi <michal.jabi@gmail.com>
2023-11-28 09:15:38 +0100

    8.8.4 solved - using 'accept-language'

--- src/middleware/language-extractor.middleware.ts
+++ src/middleware/language-extractor.middleware.ts
@@ -1,10 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+// 8.8.4 biblioteka obsługująca (q-factor weighting)
+import acceptLanguage from 'accept-language';
+// fallback języka do en, jeśli żaden nieustawiony (bo en pierwszy!)
+acceptLanguage.languages(['en', 'pl']);
+
 @Injectable()
 export class LanguageExtractorMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    req['language'] = req.headers['accept-language'] || 'en'; // fallback języka do en, jeśli żaden nie ustawiony
+    req['language'] = acceptLanguage.get(req.headers['accept-language']);
     next();
   }
 }
