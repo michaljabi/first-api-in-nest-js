a781a0fca5d2217781209a0b49077b9a2832d511
Jabi <michal.jabi@gmail.com>
2023-11-30 11:45:53 +0100

    10.4 first seed - 01_categories.ts

---
+++ src/database/seeds/01_categories.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+  // Deletes ALL existing entries
+  await knex('categories').del();
+
+  // Inserts seed entries
+  await knex('categories').insert([
+    { id: 1, name: 'Groceries' },
+    { id: 2, name: 'Cosmetics' },
+    { id: 3, name: 'Toys' },
+    { id: 4, name: 'Dairy' },
+    { id: 5, name: 'Fashion' },
+    { id: 6, name: 'Electronics' },
+    { id: 7, name: 'Games' },
+  ]);
+}
