a50613eb83b33b570af55b9fa4b0b518793e8763
Jabi <michal.jabi@gmail.com>
2023-11-30 19:04:02 +0100

    fix: order should have title.

--- src/orders/model/order.interface.ts
+++ src/orders/model/order.interface.ts
@@ -2,6 +2,7 @@ import { ProductInOrder } from './product-in-order.interface';
 
 export interface Order {
   id: number;
+  title: string;
   madeAt: Date;
   products: ProductInOrder[];
   status: 'OPENED' | 'IN_PROGRESS' | 'SHIPPED' | 'CLOSED';
