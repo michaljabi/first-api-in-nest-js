03122f0e1b028761f5e87e98e2faf09de68c1679
Jabi <michal.jabi@gmail.com>
2023-12-02 07:34:18 +0100

    12.0.2 - provide OrderModel to Module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { ProductModule } from '../product/product.module';
+import { OrderModel } from './model/order.model';
 
 @Module({
   imports: [ProductModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [
+    OrdersService,
+    {
+      provide: 'OrderModel',
+      useValue: OrderModel,
+    },
+  ],
 })
 export class OrdersModule {}
