69e919a1197b5f9df2e740a1186ed17e9812814d
Jabi <michal.jabi@gmail.com>
2023-12-03 05:02:46 +0100

    12.1 - hermetic DB connection.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,25 +1,21 @@
-import { Global, Logger, Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import knex from 'knex';
 import knexConfig from '../../knexfile';
 import { Model } from 'objection';
 
 const logger = new Logger('DbConnection');
-// potrzebujemy referencji, aby potem wrzucić ją do exports!
-const knexProvider = {
-  provide: 'DbConnection', // 'DbConnection' to nasz pomysł na nazwę tokena, może być dowolna!
-  useFactory: async (): Promise<knex.Knex> => {
-    const connection = knex(knexConfig['development']);
-    logger.log('Knex connected');
-    // Jeśli chcesz, podejrzyj sobie, co dokładnie importujemy:
-    // logger.debug(knexConfig['development'])
-    Model.knex(connection);
-    return connection;
-  },
-};
 
 @Module({
-  providers: [knexProvider],
-  exports: [knexProvider], // Udostępnij provider
+  providers: [
+    {
+      provide: 'DbConnection',
+      useFactory: async (): Promise<knex.Knex> => {
+        const connection = knex(knexConfig['development']);
+        logger.log('Knex connected');
+        Model.knex(connection);
+        return connection;
+      },
+    },
+  ],
 })
-@Global() // Chcemy, żeby każdy, kto poprosi o połączenie, dostał provider!
 export class DatabaseModule {}
