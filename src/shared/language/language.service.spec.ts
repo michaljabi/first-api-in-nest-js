98489c34110896863775583fad7594f1729e1215
Jabi <michal.jabi@gmail.com>
2023-12-06 12:37:23 +0100

    15.3 first test

--- src/shared/language/language.service.spec.ts
+++ src/shared/language/language.service.spec.ts
@@ -15,4 +15,8 @@ describe('LanguageService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return languages: en,pl as supported by this project', () => {
+    expect(service.supportedLanguages()).toEqual(['en', 'pl']);
+  });
 });
