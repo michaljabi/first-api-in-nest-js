74e5670d31f6e4aebc3283fd12ab3c997ebf54da
Jabi <michal.jabi@gmail.com>
2023-12-07 01:30:55 +0100

    15.5.3 - spy on findProduct throw exception

--- src/product/products/products.service.spec.ts
+++ src/product/products/products.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
 import { CategoriesService } from '../categories/categories.service';
 import { ProductModel } from './product.model';
+import { BadRequestException } from '@nestjs/common';
 
 describe('ProductsService', () => {
   let service: ProductsService;
@@ -43,5 +44,21 @@ describe('ProductsService', () => {
 
     //Assert
     expect(product).toEqual(foundProduct);
+  });
+
+  it('should throw BadRequestException if product is out of stock', async () => {
+    //Arrange
+    const id = 2;
+    const quantity = 5;
+    const foundProduct = { id, stock: 1 } as ProductModel;
+
+    jest
+      .spyOn(service, 'findProduct')
+      .mockReturnValue(Promise.resolve(foundProduct));
+
+    //Assert
+    await expect(service.checkProductOnStock(id, quantity)).rejects.toEqual(
+      new BadRequestException('Product :2 is out of stock.'),
+    );
   });
 });
