352deefc851cc857c6a1f176ffe8af2dd3af056e
Jabi <michal.jabi@gmail.com>
2023-12-07 02:58:20 +0100

    15.6.1 fix LanguageExtractorMiddleware spec.

--- src/middleware/language-extractor.middleware.spec.ts
+++ src/middleware/language-extractor.middleware.spec.ts
@@ -1,7 +1,27 @@
 import { LanguageExtractorMiddleware } from './language-extractor.middleware';
+import { Test, TestingModule } from '@nestjs/testing';
+import { LanguageService } from '../shared/language/language.service';
 
 describe('LanguageExtractorMiddleware', () => {
+  let middleware: LanguageExtractorMiddleware;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: LanguageService,
+          useValue: {
+            supportedLanguages: jest.fn().mockReturnValue(['en']),
+          },
+        },
+        LanguageExtractorMiddleware,
+      ],
+    }).compile();
+
+    middleware = module.get(LanguageExtractorMiddleware);
+  });
+
   it('should be defined', () => {
-    expect(new LanguageExtractorMiddleware()).toBeDefined();
+    expect(middleware).toBeDefined();
   });
 });
