74bda5d97297f0ddaeae77101dfe8b908a0fcacb
Jabi <michal.jabi@gmail.com>
2023-12-07 03:02:49 +0100

    15.6.2 fix AllErrorsFilter spec

--- src/errors/all-errors.filter.spec.ts
+++ src/errors/all-errors.filter.spec.ts
@@ -1,7 +1,25 @@
 import { AllErrorsFilter } from './all-errors.filter';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
 
 describe('AllErrorsFilter', () => {
+  let filter: AllErrorsFilter;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: ConfigService,
+          useValue: {},
+        },
+        AllErrorsFilter,
+      ],
+    }).compile();
+
+    filter = module.get(AllErrorsFilter);
+  });
+
   it('should be defined', () => {
-    expect(new AllErrorsFilter()).toBeDefined();
+    expect(filter).toBeDefined();
   });
 });
